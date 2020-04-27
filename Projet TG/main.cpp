58a2b81e8150ede5c59ce789139a000829b0b9e2
rosibool <61508411+rosistreet@users.noreply.github.com>
2020-04-27 17:30:19 +0200

    getters/setters

--- Projet TG/main.cpp
+++ Projet TG/main.cpp
@@ -3,10 +3,13 @@
 
 int main()
 {
-Graphe G("Load Graph/graphe_etoile1_topo.txt");///charge le format topologique
-G.charger_ponderation("Load Graph/graphe_etoile1_pond.txt");
-G.afficher();
-//G.afficher_matrice_indice();
-
+    ///charger graphe
+    //chargement du format topologique du graphe
+    Graphe G("Load Graph/graphe_etoile1_topo.txt");
+    //pondération
+    G.charger_ponderation("Load Graph/graphe_etoile1_pond.txt");
+    ///afficher graphe
+    G.afficher();
+    //G.afficher_matrice_indice();
     return 0;
 }
--- Projet TG/src/Sommet.cpp
+++ Projet TG/src/Sommet.cpp
@@ -12,3 +12,30 @@ Sommet::~Sommet()
 {
     //dtor
 }
+
+
+///getters
+int Sommet::get_indice() const
+{
+    return m_indice;
+}
+
+std::string Sommet::get_nom() const
+{
+    return m_nom;
+}
+int Sommet::get_x() const
+{
+    return xs;
+}
+int Sommet::get_y() const
+{
+    return ys;
+}
+
+///setters
+
+void Sommet::ajouter_adjacent(Sommet* s)
+{
+    sommet_adjacent.push_back(s);
+}
