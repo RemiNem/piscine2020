e67d390760bd5f47b71e6b097204f20b5eb3cef0
rosibool <61508411+rosistreet@users.noreply.github.com>
2020-04-27 15:07:40 +0200

    update main
    j'ai mit des tab

--- Projet TG/main.cpp
+++ Projet TG/main.cpp
@@ -5,8 +5,8 @@ int main()
 {
 Graphe G("Graphe topo/graphe_etoile1_topo.txt");///charge le format topologique
 
-G.afficher();
-//G.afficher_matrice_indice();
+    G.afficher();
+    //G.afficher_matrice_indice();
 
     return 0;
 }
